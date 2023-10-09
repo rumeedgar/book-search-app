5d1cea82edd48873f414747dc4f7149ab23b9f46
rume edgar <rumeedgar@icloud.com>
2023-10-09 14:59:30 +0100

    application update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,32 @@ import { useState } from "react";
 import axios from "axios";
 
 function App() {
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
+  const [apiKey, setApiKey] = useState(
+    "AIzaSyBjvRbisLc3Vh85QkkehF3D4ufLw9v7St4"
+  );
+
+  function handleChange(event) {
+    const book = event.target.value;
+
+    setBook(book);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(book);
+  }
+
   return (
     <div className="container">
-      <form>
+      <h1>Book Finder</h1>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <input
             type="text"
+            onChange={handleChange}
             className="form-control mt-10"
             placeholder="search for books"
             autoComplete="off"
