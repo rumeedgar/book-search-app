a6eb47195021ec08f67cf75e77d0119da6b823be
rume edgar <rumeedgar@icloud.com>
2023-10-09 14:35:08 +0100

    installed bootstrap and created a search bar and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container">
+      <form>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control mt-10"
+            placeholder="search for books"
+            autoComplete="off"
+          />
+        </div>
+        <button type="submit" className="btn btn-danger mt-3">
+          search
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
